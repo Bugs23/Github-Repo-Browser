f61362fcd221475f4bbbf9e44c5af7e49df7d02f
Raymond <raymondurrutia2@yahoo.com>
2024-10-29 12:47:41 -0400

    Added functionality that allows user to enter a github username and fetch a list of that users public repos

--- src/components/RepoCard.jsx
+++ src/components/RepoCard.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import { FaStar } from "react-icons/fa6";
 
 
-export default function RepoCard() {
+export default function RepoCard({name, description, stars, link}) {
   return (
-    <div className='card'>
-        <div className='name-container'>
-            <h3 className='name'>Facebook</h3>
-            <span className='star-count'><FaStar className='star-icon' />16</span>
+    <div className="card">
+        <div className="name-container">
+            <h3 className="name">{name}</h3>
+            <span className="star-count"><FaStar className="star-icon" />{stars}</span>
         </div>
-        <p className='desciption'>An unprecedented series of violent tornadoes is sweeping across Oklahoma. Tornado chasers, headed by Dr. Jo Harding, attempt to release a groundbreaking device that will allow them to track them and create a more advanced warning system. They are joined by Jo's soon to be ex-husband Bill, a former tornado chaser himself, and his girlfriend Melissa.</p>
-        <button>View Repository</button>
+        <p className="desciption">{description}</p>
+        <a href={link} className="button" target="_blank">View Repository</a>
     </div>
   )
 }
--- src/components/RepoSearch.jsx
+++ src/components/RepoSearch.jsx
@@ -1,26 +1,96 @@
-import React from 'react'
+import React, {useState} from 'react'
 import RepoCard from './RepoCard'
 
-const BASE_URL = "https://api.github.com/users/bugs23/repos"
-
 export default function RepoSearch() {
-  return (
-    <div className='repo-search'>
-        <form>
-            <input 
-                className="input" 
-                type="text" 
-                name="query" 
-                placeholder="i.e. Facebook"
+
+    // Store and update the username that's typed in the input box
+    const [username, setUsername] = useState("")
+
+    // Store and update the repos fetched from Github
+    const [repos, setRepos] = useState([])
+
+    // Store and set any error that occurs during the API call
+    const [error, setError] = useState(null)
+
+    // handleSubmit triggers when a user clicks the form button
+    const handleSubmit = async (e) => {
+        // Prevent a page refresh
+        e.preventDefault()
+
+        // Clear previous errors so it is clear for each fetch
+        setError(null)
+
+        // Clear previous repos fetched
+        setRepos([])
+
+        // Check if a username has been entered
+        if (username.trim() === "") {
+            setError("Please enter a GitHub username.")
+            return
+        }
+
+        try {
+            const response = await fetch(`https://api.github.com/users/${username}/repos`)
+            
+            // Check if the user's not found or there's a server error
+            if (!response.ok) {
+                if (response.status === 400) {
+                    throw new Error("User not found")
+                } else {
+                    throw new Error("Failed to fetch repositories. Please try again later.")
+                }
+            }
+
+            // Convert response to json and store it in a variable called data
+            const data = await response.json()
+
+            // Check if user has no public repositories
+            // If the data array is empty, it means the username exists but has no public repos
+            if (data.length === 0) {
+                setError("This user has no public repositories.")
+            } else {
+                // Update repo's array with the list of repos returned from retch
+                setRepos(data)
+            }
+
+        // If there's an error during fetching, update the error state with the error message
+        } catch (err) {
+            setError(err.message)
+            console.log(err.message)
+        }
+
+    }
+
+    const repoCardElements = repos.map((repo) => {
+        return (
+            <RepoCard 
+                key={repo.id}
+                name={repo.name}
+                description={repo.description}
+                stars={repo.stargazers_count}
+                link={repo.html_url}
             />
-            <button className="button" type="submit">Fetch Repositories</button>
-        </form>
-        <div className='repo-card-list'>
-            <RepoCard />
-            <RepoCard />
-            <RepoCard />
-            <RepoCard />
+        )
+    })
+
+    return (
+        <div className='repo-search'>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="text" 
+                    name="username" 
+                    placeholder="Enter GitHub username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <button type="submit">Fetch Repos</button>
+            </form>
+            {/* Display the error message if it exists */}
+            {error && <p className="error-message">{error}</p>}
+
+            <div className='repo-card-list'>
+                {repoCardElements}
+            </div>
         </div>
-    </div>
-  )
+    )
 }
